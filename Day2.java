0845b59dd5b4dc0dc86cd63a561dfc2c19bdd528
sitisyariah <sitisyariah637@gmail.com>
2025-09-09 13:43:54 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,7 @@
+public class Day2.java {
+    public static void main(String[] args) {
+        System.out.println("D0224012");
+        System.out.print("Haii!! ");
+        System.out.printf("%s","SITI SYARIAH");
+    }
+}
