61eaac4a3067684a261f6a1b07ecb53a0de63bb6
sitisyariah <sitisyariah637@gmail.com>
2025-09-10 14:14:30 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,19 @@
+         /**
+         * Contoh penggunaan komentar di Java
+         */
+        public class Day3.java {
+            public static void main(String[] args) {
+
+                // Komentar satu baris
+                int a = 5; // variabel a bernilai 5
+                int b = 3; // variabel b bernilai 3
+
+           /* Komentar multi baris
+           Menjumlahkan dua variabel */
+
+                int hasil = a + b;
+
+                // Menampilkan hasil
+                System.out.println("Hasil: " + hasil);
+            }
+        }
