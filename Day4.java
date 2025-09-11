870110e906907b2133da890dc5868f7df868ebc0
sitisyariah <sitisyariah637@gmail.com>
2025-09-11 19:55:04 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,21 @@
+public class Day4.java {
+    public static void main(String[] args) {
+
+                // Contoh penggunaan \n untuk pindah teks kemudian jadi baris baru (baris baru)
+                System.out.println("=== Biodata ===\n");
+
+                // Contoh penggunaan \t untuk memberikan spasi yang panjang (tab)
+                System.out.println("Nama:\tSiti Syariah");
+                System.out.println("Kelas:\tXII RPL");
+                System.out.println("Hobi:\tCoding\n");
+
+                // Contoh penggunaan \" untuk menampilkan tanda kutip dua di konsole (kutip ganda)
+                System.out.println("Kata Siti: \"Belajar Java itu menyenangkan!\"\n");
+
+                // Contoh penggunaan \' untuk menampilkan tanda kutip satu di konsole (kutip tunggal)
+                System.out.println("Hari ini adalah Jum\'at.\n");
+
+                // Contoh penggunaan \b untuk menghapus huruf sebelumnya (backslash)
+                System.out.println("Lokasi file: C:\\Users\\Siti\\Documents");
+            }
+        }
