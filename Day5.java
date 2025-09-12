39ff7af5a761d30591456338dd0962c22011da49
sitisyariah <sitisyariah637@gmail.com>
2025-09-12 19:17:31 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,16 @@
+public class Day5.java {
+    public static void main(String[] args) {
+
+                byte angkaByte = 50; // Tipe data byte rentang dari -128 sd 172
+                System.out.println("Nilai byte: " + angkaByte);
+
+                short angkaShort = 100; // Tipe data short memiliki rentang dari -32, 768 sd 32, 76
+                System.out.println("Nilai short: " + angkaShort);
+
+                int angkaInt = 1011; // Tipe data int yang memiliki rentang -2^31 sd 2^31-1
+                System.out.println("Nilai int: " + angkaInt);
+
+                long angkaLong = 900000; // Tipe data long (harus diakhiri huruf L) rentangnya -2^63 sd 2^6
+                System.out.println("Nilai long: " + angkaLong);
+            }
+        }
