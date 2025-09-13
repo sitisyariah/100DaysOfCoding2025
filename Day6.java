1b7beff3089e51c41d16b45ff97a64dfa0ddac9d
sitisyariah <sitisyariah637@gmail.com>
2025-09-13 18:30:26 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Day6.java {
+    public static void main(String[] args) {
+        double a = 90.000;
+        float b = 50.5f;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
