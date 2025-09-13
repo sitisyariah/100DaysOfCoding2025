f444e8719c4a94eea73946ea164825cd268a5666
sitisyariah <sitisyariah637@gmail.com>
2025-09-13 18:39:28 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+ublic class Day7.java {
+    public static void main(String[] args) {
+  
+        char inisial ='p';
+        boolean pernyataan= true;
+
+        System.out.println("perempuan:"+inisial);
+        System.out.println(pernyataan);
+        }
+    }
