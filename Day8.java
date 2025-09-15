477c1ee00132420097e75bcf605af280c5b22245
sitisyariah <sitisyariah637@gmail.com>
2025-09-15 21:19:15 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,11 @@
+public class Day8.java {
+    public static void main(String[] args) {
+        // Membuat variabel String
+        String hy = "hay semua";
+        String nama = "saya siti";
+
+        // Menampilkan isi String
+        System.out.println("hy: " + hy);
+        System.out.println("nama: " + nama);
+    }
+}
