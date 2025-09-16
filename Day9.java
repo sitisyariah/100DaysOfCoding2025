2cc60fd79f845e55dfd293380ee92edeb6e26faf
sitisyariah <sitisyariah637@gmail.com>
2025-09-16 22:27:11 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,24 @@
+public class Day9.java {
+    public static void main(String[] args) {
+
+            // Deklarasi
+            int nim;
+            String nama;
+
+            // Inisialisasi
+            nim = 3;
+            nama = "Siti";
+
+            // Cetak nilai awal
+            System.out.println("Nilai awal angka: " + nim);
+            System.out.println("Nama awal: " + nama);
+
+            // Update variabel
+            nim = 5;
+            nama = "siti Syariah";
+
+            // Cetak nilai setelah update
+            System.out.println("Nilai setelah update angka: " + nim);
+            System.out.println("Nama setelah update: " + nama);
+        }
+    }
