1d81087f0a20b988d5648dacb49d646a2214c567
sitisyariah <sitisyariah637@gmail.com>
2025-09-17 23:08:45 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,9 @@
+apublic class Day10.java {
+    public static void main(String[] args) {
+                final byte angka = 19;
+                final int huruf;
+                huruf=20;  // nilai yang didalam variabel tetap tidak dapat di update karena menggunakan final
+                System.out.println(huruf);
+                System.out.println(angka);
+            }
+        }
