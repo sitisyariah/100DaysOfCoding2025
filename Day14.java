d125119804ff7ae47905880275194e943bbd62e0
sitisyariah <sitisyariah637@gmail.com>
2025-09-21 21:39:58 +0900

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,15 @@
+public class Day14.java {
+    public static void main(String[] args) {
+
+        String nilai1 = "19"; // inisialisasi
+        String nilai2 = "20";
+        String nilai3 = "5";
+
+        int nilai4 = Integer.parseInt(nilai1); //untuk mengubah string ke integer
+        double nilai5 = Double.parseDouble(nilai2);// mengubah string ke double
+        byte nilai6 = Byte.parseByte(nilai3); // mengubah string ke byte
+
+        System.out.println(nilai4 + nilai5 - nilai6); // menampilkan outputnya
+
+    }
+}
