27dda80b3f4c78855048e85a63bd2fea78325654
sitisyariah <sitisyariah637@gmail.com>
2025-09-22 20:42:17 +0800

    Day15.java

--- Day15.java
+++ Day15.java
@@ -3,15 +3,10 @@ public class Day15.java {
         int angka1 = 10;   // angka pertama
         byte angka2 = 4;    // angka kedua
 
-        // Operasi penjumlahan
-        int tambah = angka1 + angka2;
-
-        // Operasi pengurangan
-        int kurang = angka1 - angka2;
+        int tambah = angka1 + angka2;// Operasi penjumlahan
+        int kurang = angka1 - angka2; // Operasi pengurangan
 
         // mencetak output
-        System.out.println("Angka 1 = " + angka1);
-        System.out.println("Angka 2 = " + angka2);
         System.out.println("Hasil Penjumlahan = " + tambah);
         System.out.println("Hasil Pengurangan = " + kurang);
     }
