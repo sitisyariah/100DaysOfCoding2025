1c106e565dee7a09254e846d24153f1b2b7decbf
sitisyariah <sitisyariah637@gmail.com>
2025-09-22 20:21:05 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,18 @@
+public class Day15.java {
+    public static void main(String[] args) {
+        int angka1 = 10;   // angka pertama
+        int angka2 = 4;    // angka kedua
+
+        // Operasi penjumlahan
+        int tambah = angka1 + angka2;
+
+        // Operasi pengurangan
+        int kurang = angka1 - angka2;
+
+        // mencetak output
+        System.out.println("Angka 1 = " + angka1);
+        System.out.println("Angka 2 = " + angka2);
+        System.out.println("Hasil Penjumlahan = " + tambah);
+        System.out.println("Hasil Pengurangan = " + kurang);
+    }
+}
