43c83d17f55e67d6c7b8b5b91509449e9baca277
sitisyariah <sitisyariah637@gmail.com>
2025-09-23 18:44:55 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,14 @@
+public class Day16.java {
+    public static void main(String[] args) {
+
+        int angka1 = 10;
+        int angka2 = 5;
+
+        int hasilKali = angka1 * angka2; // operator aritmatika perkalian *
+        int hasilBagi = angka1 / angka2; // operator aritmatika pembagian /
+
+        // ouputnya
+        System.out.println("Hasil perkalian : " + hasilKali); //outputnya 50
+        System.out.println("Hasil pembagian : " + hasilBagi); //outputnya 2
+    }
+}
