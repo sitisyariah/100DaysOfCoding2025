3310a2ac5122414451f67d7334b510af1ed3be0f
sitisyariah <sitisyariah637@gmail.com>
2025-09-24 17:42:20 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,12 @@
+public class Day17.java {
+    public static void main(String[] args) {
+        int angka1 = 19;
+        int angka2 = 5;
+        int hasil = angka1 % angka2; // Menghitung sisa hasil bagi
+
+        System.out.println("angka1     : " + angka1); //mencetak angka 
+        System.out.println("angka2     : " + angka2);
+        System.out.println("Sisa bagi  : " + hasil); // mencetak hasil sisa bagi
+        }
+    }
+
