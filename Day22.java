5e6a0c82ad739940b0c615cc1c96b8058bb2ae68
sitisyariah <sitisyariah637@gmail.com>
2025-09-29 21:01:50 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,12 @@
+public class Day22.java {
+    public static void main(String[] args) {
+        int sisi = 4;
+      
+        // menghitung luas persegi
+        int luas = sisi * sisi;
+
+        // menampilkan hasil 
+        System.out.println("Sisi persegi = " + sisi);
+        System.out.println("Luas persegi = " + luas);
+    }
+}
