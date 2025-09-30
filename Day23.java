8f825ee2c3aee091ef4bd14928a97a9a897ceafe
sitisyariah <sitisyariah637@gmail.com>
2025-09-30 20:22:17 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,13 @@
+public class Day23.java {
+    public static void main(String[] args) {
+        int panjang = 4;
+        int lebar = 2;
+        
+        int luas = panjang * lebar; // menghitung luas
+
+        // menampilkan hasil
+        System.out.println("Panjang = " + panjang);
+        System.out.println("Lebar   = " + lebar);
+        System.out.println("Luas persegi panjang = " + luas);
+    }
+}
