00346e55f9bc6b02c256d76c49e44ec4cf862b44
sitisyariah <sitisyariah637@gmail.com>
2025-10-02 20:44:51 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,12 @@
+public class Day25.java{
+    public static void main(String[] args) {
+
+        int i = 4;
+        //Increment
+        i++;
+        System.out.println("Nialai a setelah di increment = " + i);
+        // Decrement
+        i--;
+        System.out.println("Nilai a setelah di decrement = " + i);
+    }
+}
