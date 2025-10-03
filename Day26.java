a769d62ac394d9d7487ca62ecc27b2d82f5d958a
sitisyariah <sitisyariah637@gmail.com>
2025-10-03 18:54:51 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,21 @@
+public class Day26.java {
+    public static void main(String[] args) {
+        int x = 10;  // penugasan awal
+        System.out.println("Nilai awal x = " + x);
+
+        x += 5;  // sama dengan x = x + 5
+        System.out.println("Setelah x += 5 : " + x);
+
+        x -= 3;  // sama dengan x = x - 3
+        System.out.println("Setelah x -= 3 : " + x);
+
+        x *= 2;  // sama dengan x = x * 2
+        System.out.println("Setelah x *= 2 : " + x);
+
+        x /= 4;  // sama dengan x = x / 4
+        System.out.println("Setelah x /= 4 : " + x);
+
+        x %= 3;  // sama dengan x = x % 3
+        System.out.println("Setelah x %= 3 : " + x);
+    }
+}
