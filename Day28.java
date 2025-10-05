2d5f4a13cbd23ebab9a6aed6ae1facca0dec1e79
sitisyariah <sitisyariah637@gmail.com>
2025-10-05 19:30:11 +0900

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,15 @@
+public class Day28.java {
+        public static void main(String[] args) {
+            int a = 5;
+            int b = 7;
+
+            // Menggunakan ==
+            System.out.println("Apakah a == b " + (a == b));
+            System.out.println("Apakah a == 5 " + (a == 5)); 
+
+            // Menggunakan !=
+            System.out.println("Apakah a != b " + (a != b)); 
+            System.out.println("Apakah a != 5 " + (a != 5)); 
+        }
+    }
+
