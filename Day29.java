df3baa86ff4e98e70cefa9ca6054625994547d15
sitisyariah <sitisyariah637@gmail.com>
2025-10-06 21:14:49 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,10 @@
+public class Day29.java {
+    public static void main(String[] args) {
+        int angka1 = 2;
+        int angka2 = 4;
+      
+        // operator < dan >
+        System.out.println("apakah angka1 lebih besar dari angka2? : " + (angka1 < angka2));//true
+        System.out.println("apakah angka1 lebih kecil dari angka2? : " + (angka1 > angka2));//false
+    }
+}
