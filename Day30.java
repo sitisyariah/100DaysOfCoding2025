af049ed7179a5a8d7c08128074b19d9bcb3fb682
sitisyariah <sitisyariah637@gmail.com>
2025-10-07 18:49:48 +0800

    Day30.java

--- Day30.java
+++ Day30.java
@@ -4,9 +4,9 @@ public class Day30.java {
         int angka1 = 2;
         int angka2 = 4;
         //operator perbandingan >= dan <=
-        System.out.println("apakah angka1 lebih kecil sama dengan angka2? :" + (angka1 >= angka2));
-        System.out.println("apakah angka1 lebih besar sama dengan angka2? :" + (angka1 <= angka2));
-        System.out.println("apakah angka2 lebih kecil sama dengan angka1? :" + (angka2 >= angka1));
-        System.out.println("apakah angka2 lebih besar sama dengan angka1? :" + (angka2 <= angka1));
+        System.out.println("apakah angka1 lebih kecil sama dengan angka2? :" + (angka1 <= angka2));
+        System.out.println("apakah angka1 lebih besar sama dengan angka2? :" + (angka1 >= angka2));
+        System.out.println("apakah angka2 lebih kecil sama dengan angka1? :" + (angka2 <= angka1));
+        System.out.println("apakah angka2 lebih besar sama dengan angka1? :" + (angka2 >= angka1));
     }
 }
