aa27a67a0ebc40a601c3a9ae90470a28ad607cd6
sitisyariah <sitisyariah637@gmail.com>
2025-10-08 23:13:44 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,13 @@
+public class Day31.java {
+    public static void main(String[] args) {
+        int nilai1 = 80;
+        int nilai2 = 70;
+
+        // Kondisi: nilai1 > 75 DAN nilai2 > 60
+        boolean lulus = (nilai1 > 75) && (nilai2 > 60);
+
+        System.out.println("Nilai 1 = " + nilai1);
+        System.out.println("Nilai 2 = " + nilai2);
+        System.out.println("Apakah lulus? " + lulus);
+    }
+}
