06d36c30c9d048f03e37414aebd04328ba8f063b
sitisyariah <sitisyariah637@gmail.com>
2025-10-10 20:16:57 +0800

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,7 @@
+public class Day33.java{
+        public static void main(String[] args) {
+            boolean hujan = true;      // artinya: sedang hujan
+            System.out.println("apakah tidak hujan : " + !hujan); // !hujan artinya: tidak hujan
+            System.out.println("apakah hujan : " + hujan); // true
+        }
+    }
