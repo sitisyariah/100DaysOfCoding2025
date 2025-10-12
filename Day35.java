13db4ee4c4be99a4eb50ccc589999d7d3d8fdf15
sitisyariah <sitisyariah637@gmail.com>
2025-10-12 21:19:00 +0900

    Day35.java

---
+++ Day35.java
@@ -0,0 +1,9 @@
+public class Day35 {
+        public static void main(String[] args) {
+            int nilai = 80;
+
+            if (nilai >= 75) {
+                System.out.println("Selamat, kamu lulus!");
+            }
+        }
+    }
