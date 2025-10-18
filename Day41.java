db1afc303b51919cc2cd3ba37e9e1f4c438d4833
sitisyariah <sitisyariah637@gmail.com>
2025-10-18 19:57:43 +0800

    Day41.java

---
+++ Day41.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+    public class Day41 {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("=== Program Menentukan Gaji Bersih ===");
+            System.out.print("Masukkan gaji pokok: ");
+            double gajiPokok = input.nextDouble();
+
+            System.out.print("Masukkan tunjangan: ");
+            double tunjangan = input.nextDouble();
+
+            System.out.print("Masukkan potongan: ");
+            double potongan = input.nextDouble();
+
+            double gajiKotor = gajiPokok + tunjangan;
+            double gajiBersih = gajiKotor - potongan;
+
+            System.out.println("\n--- Hasil Perhitungan ---");
+            System.out.println("Gaji Pokok  : Rp " + gajiPokok);
+            System.out.println("Tunjangan   : Rp " + tunjangan);
+            System.out.println("Potongan    : Rp " + potongan);
+            System.out.println("Gaji Bersih : Rp " + gajiBersih);
+        }
+    }
