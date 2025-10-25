3f486936a1f88ed813f22c9aa0a30fe3b86bdd49
sitisyariah <sitisyariah637@gmail.com>
2025-10-25 22:01:33 +0800

    Day48.java

---
+++ Day48.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Day48.java {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== KALKULATOR SEDERHANA ===");
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = input.nextDouble();
+        double hasil;
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '/':
+                if (angka2 != 0) {
+                    hasil = angka1 / angka2;
+                    System.out.println("Hasil: " + hasil);
+                } else {
+                    System.out.println("Error: Pembagian dengan nol tidak diperbolehkan!");
+                }
+                break;
+            default:
+                System.out.println("Operator tidak dikenal!");
+                break;
+        }
+    }
+}
