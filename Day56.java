2ca961475b3003e652093d716f2253c6be8a7df6
sitisyariah <sitisyariah637@gmail.com>
2025-11-02 18:32:19 +0800

    Do-while

---
+++ Day56.java
@@ -0,0 +1,9 @@
+public class Day56{
+    public static void main(String[] args) {
+        int i = 1; 
+        do {
+            System.out.println("Nilai i: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
