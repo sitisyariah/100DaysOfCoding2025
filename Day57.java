ebce64bd7f088b2b32c8b674ebfe3d2f3c7861c0
sitisyariah <sitisyariah637@gmail.com>
2025-11-03 18:44:33 +0800

    Nested loop

---
+++ Day57.java
@@ -0,0 +1,9 @@
+public class Day57{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {             // Loop luar
+            for (int j = 1; j <= 2; j++) {         // Loop dalam
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
