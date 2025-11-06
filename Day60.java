4803775f0ed8b5d141539ee064c789e991489b47
sitisyariah <sitisyariah637@gmail.com>
2025-11-06 12:50:46 +0800

    Mencetak angka ganjil N-1 dan genap N-1

---
+++ Day60.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Day60 {
+    public static void main (String[] args){
+        Scanner input = new Scanner (System.in);
+        System.out.print("Masukkan nilai N : ");
+        int n = input.nextInt();
+
+        System.out.print("Mencetak angka ganjil N sampai 1 : ");
+        for(int i = n; i >= 1; i--){
+            if(i % 2 == 1) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        System.out.print("Mencetak angka genap N sampai 1 : ");
+        for(int j = n; j >= 1; j--){
+            if(j % 2 == 0) {
+                System.out.print(j + " ");
+            }
+        }
+    }
+}
