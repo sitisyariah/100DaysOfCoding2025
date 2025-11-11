a1997a905b5039b182d8d9530ffd08e66f7c3083
sitisyariah <sitisyariah637@gmail.com>
2025-11-11 20:15:52 +0800

    Faktorial

--- Day65.java
+++ Day65.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BilanganFaktorial {
+public class Day65 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);   // Membuat objek Scanner untuk input
 
