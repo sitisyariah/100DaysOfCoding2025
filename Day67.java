25b377a953715ee3950ebd98e20a32cb8160e0a0
sitisyariah <sitisyariah637@gmail.com>
2025-11-13 20:00:02 +0800

    Mencetak pola horizontal

--- Day67.java
+++ Day67.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-public class HorizontalStars {
+public class Day67 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan jumlah bintang: ");
+        System.oeut.print("Masukkan jumlah bintang: ");
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
