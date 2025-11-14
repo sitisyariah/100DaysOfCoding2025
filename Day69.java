09e329b46ce7557b4c2457b173987c18ef2bd3d8
sitisyariah <sitisyariah637@gmail.com>
2025-11-14 20:18:59 +0800

    membuuat pola persegi

---
+++ Day69.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Day69 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah baris: ");
+        int baris = input.nextInt();
+
+        System.out.print("Masukkan jumlah kolom: ");
+        int kolom = input.nextInt();
+
+        for (int i = 0; i < baris; i++) {          // perulangan baris
+            for (int j = 0; j < kolom; j++) {      // perulangan kolom
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
