5f7565c4caea66b5450a6742f53715821c536bc9
sitisyariah <sitisyariah637@gmail.com>
2025-11-17 20:43:30 +0800

    Update Day71.java

--- Day71.java
+++ Day71.java
@@ -7,11 +7,11 @@ public class Day71 {
         System.out.print("Masukkan nilai n: ");
         int n = input.nextInt();
 
-        for (int i = n; i >= 1; i--) {        // baris dimulai dari n ke 1
-            for (int j = 1; j <= i; j++) {    // mencetak bintang sebanyak nilai i
+        for (int i = n; i >= 1; i--) {       
+            for (int j = 1; j <= i; j++) {    
                 System.out.print("*");
             }
-            System.out.println();             // pindah ke baris berikutnya
+            System.out.println();       
         }
     }
 }
