b9fae542866ea418a55c06b78d1a6d5fb289c315
sitisyariah <sitisyariah637@gmail.com>
2025-11-19 18:53:16 +0800

    Perulangan di while

--- Day73.java
+++ Day73.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MenuDoWhile {
+public class Day73{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int pilihan;
